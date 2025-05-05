c69f1a4217655d758704566e083593a005493ddf
yuvalkonimbo <yuval@konimbo.co.il>
2025-05-05 13:51:51 +0300

    get from DB
    in route.ts the endpoint get the Products objects from the DB.

---
+++ src/app/api/feeds/[slug]/route.ts
@@ -0,0 +1,44 @@
+import { getPayload } from 'payload'
+import configPromise from '@payload-config'
+import { NextRequest, NextResponse } from 'next/server'
+import { Liquid } from 'liquidjs'
+
+export async function GET(req: NextRequest, context: { params: { slug: string } }) {
+  // get template
+  const { slug } = context.params
+
+  const payload = await getPayload({ config: configPromise })
+
+  const feedDoc = await payload.find({
+    collection: 'feeds',
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  })
+  const feed = feedDoc.docs?.[0]
+
+  if (!feed) return new NextResponse('Feed not found', { status: 404 })
+
+  const { template } = feed
+  // ***
+
+  // get products from db
+  const productsQuery = await payload.find({
+    collection: 'products',
+    pagination: false,
+  })
+  const products = productsQuery.docs
+  const dataForTemplate = { products }
+  // ***
+
+  const engine = new Liquid()
+  const rendered = await engine.parseAndRender(template, dataForTemplate)
+
+  return new NextResponse(rendered, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
